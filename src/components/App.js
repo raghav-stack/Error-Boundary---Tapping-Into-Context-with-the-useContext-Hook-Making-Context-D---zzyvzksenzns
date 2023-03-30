aeed64b14376f34a5c30ee841f374123f1607a8d
Raghav <kumarraghav71950@gmail.com>
2023-03-30 08:42:47 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,17 @@ import React from "react";
 import "../styles/App.css";
 import Component from "./Component";
 import ErrorBoundary from "./ErrorBoundary";
-const App = () => {
 
+const App = () => {
+  
+  let throwErrorFunction;
+  let errorGenerator = () => {
+    throwErrorFunction(true);
+  };
+
+  let changeFunc = (updateError) => {
+    throwErrorFunction = updateError;
+  };
 
   return (
 
--- src/components/Component.js
+++ src/components/Component.js
@@ -3,11 +3,16 @@ import React, { useState } from "react";
 const Component = (props) => {
     
     const [error, setError] = useState(false);
+    props.change(setError);
+
+    if (error) {
+      throw new Error("My error");
+    }
 
   return (
     <>
     {    
-        error && <div>{somethingNotDefined}</div>
+        error && <div>{""}</div>
     }
     </>
   );
